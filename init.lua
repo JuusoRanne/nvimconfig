610c5776a9c9c7d402ea1577bfae0de35a690f2b
juusoranne <juuso.ranne@protonmail.com>
2024-02-03 18:01:26 +0200

    nvim conf

---
+++ init.lua
@@ -0,0 +1,32 @@
+-- basic settings
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.g.mapleader = " "
+
+-- setting up package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+local opts = {}
+
+-- get lazy configs and plugs (points to plugins.lua)
+require("lazy").setup("plugins")
+
+
+
+
